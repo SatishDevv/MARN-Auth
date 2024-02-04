35b735fa20f3ba10d3995fb9a492298dcee0a382
Satishghode <satishghode9075@gmail.com>
2024-02-04 18:59:08 +0530

    Connect to the DataBase

--- api/index.js
+++ api/index.js
@@ -1,10 +1,22 @@
 import express from 'express';
+import mongoose from 'mongoose';
+import dotenv from 'dotenv';
+dotenv.config();
 
 const app = express();
 
+mongoose.connect(process.env.MONGO_TEST)
+.then( () => {
+    console.log('connected to the mongoose database server');
+} )
+.catch( (err) =>{
+    console.log(err);
+
+} );
+
 
 app.listen(3000, () => {
-    console.log('server listening on port 3000');
-});
+        console.log('server listening on port 3000');
+    });
 
 
