5f7dc034ab2f046dbfc1b612d1ab4a5fa4fb081f
Satishghode <satishghode9075@gmail.com>
2024-02-08 01:16:11 +0530

    Complete the signin page functionality

--- client/src/pages/SignIn.jsx
+++ client/src/pages/SignIn.jsx
@@ -1,7 +1,73 @@
-import React from 'react'
+import React, { useState } from "react";
+import { Link, useNavigate } from "react-router-dom";
 
 export default function SignIn() {
+  const [formData, setFormData] = useState({});
+  const [error, setError] = useState(false);
+  const [loading, setLoading] = useState(false);
+  const navigate = useNavigate();
+
+  const handleChange = (e) => {
+    setFormData({ ...formData, [e.target.id]: e.target.value });
+  };
+
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    try {
+      setLoading(true);
+      setError(false);
+      const res = await fetch("/api/auth/sign-in", {
+        method: "POST",
+        headers: {
+          "Content-type": "application/json",
+        },
+        body: JSON.stringify(formData),
+      });
+      const data = await res.json();
+      setLoading(false);
+      if (data.success === false) {
+        setError(true);
+        return;
+      }
+      navigate("/");
+    } catch (error) {
+      setLoading(false);
+      setError(true);
+    }
+  };
+
   return (
-    <div>Signin</div>
-  )
+    <div className="p-3 max-w-lg mx-auto">
+      <h1 className="text-3xl text-center font-semibold my-7  ">Sign In</h1>
+      <form onSubmit={handleSubmit} className=" flex flex-col gap-4 ">
+        <input
+          type="email"
+          placeholder="Email"
+          required
+          id="email"
+          className=" bg-slate-100 p-3 rounded-lg"
+          onChange={handleChange}
+        />
+        <input
+          type="password"
+          placeholder="password"
+          id="password"
+          required
+          className="bg-slate-100 p-3 rounded-lg"
+          onChange={handleChange}
+        />
+        <button disabled={loading} className="bg-slate-700 rounded-lg p-3 text-white uppercase hover:opacity-90 disabled:opacity-50 ">
+          {loading ? "loading..." : "sign in"}
+        </button>
+      </form>
+      <div className="flex gap-4 pt-5">
+        <p>Dont have an account ?</p>
+        <Link to="/sign-up">
+          <span className="text-red-500">Sign up</span>
+        </Link>
+      </div>
+      <p className="text-red-500 pt-3 " > {error && 'Invalid Credentials '} </p>
+    </div>
+  );
 }
