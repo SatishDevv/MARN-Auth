85ec6b7a4564e32d772b982d909f23bdc6e7b471
Satishghode <satishghode9075@gmail.com>
2024-02-08 20:50:40 +0530

    Update the header Components  make the profile page private

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,23 +1,25 @@
-import { BrowserRouter , Routes , Route } from 'react-router-dom';
-import Home from './pages/Home'
-import About from './pages/About';
-import SignIn from './pages/SignIn';
-import SignUp from './pages/SignUp';
-import Profile from './pages/Profile';
-import Header from './components/Header';
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import About from "./pages/About";
+import SignIn from "./pages/SignIn";
+import SignUp from "./pages/SignUp";
+import Profile from "./pages/Profile";
+import Header from "./components/Header";
+import PrivateRoute from "./components/privateRoute";
 export default function App() {
   return (
     <BrowserRouter>
-    {/* Headers componentes is comman for all pages that why we adding Outside the routes and inside the BrowserRoutes  */}
+      {/* Headers componentes is comman for all pages that why we adding Outside the routes and inside the BrowserRoutes  */}
       <Header />
       <Routes>
-        <Route path='/' element={<Home />} /> 
-        <Route path='/about' element={<About />} /> 
-        <Route path='/sign-in' element={<SignIn />} /> 
-        <Route path='/sign-Up' element={<SignUp />} /> 
-        <Route path='/profile' element={<Profile />} /> 
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/sign-in" element={<SignIn />} />
+        <Route path="/sign-Up" element={<SignUp />} />
+        <Route element={<PrivateRoute />}>
+          <Route path="/profile" element={<Profile />} />
+        </Route>
       </Routes>
     </BrowserRouter>
-    
   );
 }
--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -1,5 +1,8 @@
 import { Link } from "react-router-dom";
+import { useSelector } from "react-redux";
 export default function Header() {
+  const { currentUser } = useSelector((state) => state.user )
+
   return (
     <div className="bg-slate-200">
       <div className="flex justify-between items-center max-w-6xl mx-auto p-3 ">
@@ -13,8 +16,15 @@ export default function Header() {
           <Link to='/about' >
             <li>About</li>
           </Link>
-          <Link to='/sign-in' >
-            <li>Sign In</li>
+          <Link to='/profile' >
+            {
+              currentUser ? (
+                <img className="h-8 w-8 rounded-full object-cover " src={currentUser.profilePicture} />
+              ):(
+                
+                <li>Sign In</li>
+              )
+            }
           </Link>
         </ul>
       </div>
